3fc1c71d082b33198fd65fb0652de6919ee959fb
AmiReza Hammamian <amir.astar20@gmail.com>
2022-09-08 23:12:14 +0430

    feat: add select and date navigation on datepicker

--- lib/src/datepicker/ngx-date-picker.component.ts
+++ lib/src/datepicker/ngx-date-picker.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { CalendarType, GlobalConfig, SelectModes, days, months, IDate } from '../config/datePicker-config';
-import { toJalaali } from "jalaali-js"
+import { Component, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { CalendarType, GlobalConfig, SelectModes, 
+         weekDays, months, IDate, DaysViewInMonth, ISelectedDate } from '../config/datePicker-config';
 import Calendar from '../helpers/calendar';
 import DateInfo from '../helpers/dateInfo';
 
@@ -8,6 +8,7 @@ import DateInfo from '../helpers/dateInfo';
   selector: 'ngx-date-picker',
   templateUrl: 'ngx-date-picker.component.html',
   styleUrls: ['./ngx-date-picker.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   providers: [
     Calendar
   ]
@@ -19,29 +20,123 @@ export class NgxDatePickerComponent implements OnInit, OnDestroy {
 
   selectMode: SelectModes = "days";
   calendarType: CalendarType = "gregorian";
+  months!: string[];
+  weekDays!: string[];
 
+  selectedMonthDays!: DaysViewInMonth[];
   currentDate!: IDate;
+  selectedDate!: ISelectedDate;
+  yearsStep: number = 0;
 
   constructor(
     private calandarConfig: Calendar
   ) {}
 
   ngOnInit(): void {
-    this.calendarType = this.config.calendar
-    this.calandarConfig.setCalendar(this.calendarType)
-    this.currentDate = this.getCurrentDate()
-    const { month, year } = this.currentDate
-    const res = this.calandarConfig.getChunkedDaysInSelectedMonth(month, year)
-    console.log(res);
+    this.calendarType = this.config.calendar;
+    this.months = months[this.calendarType]
+    this.weekDays = weekDays[this.calendarType]
+
+    this.calandarConfig.setCalendar(this.calendarType);
+    this.currentDate = this.getCurrentDate();    
+    this.selectedDate = {...this.currentDate}
+
+    const { month, year } = this.currentDate;
+    const selectedMonthDays = this.calandarConfig.getChunkedDaysInSelectedMonth(month!, year);
+    this.selectedMonthDays = selectedMonthDays;    
   }
 
   getCurrentDate(): IDate{
-    const currentDate = new Date()
-    const dateInfo = new DateInfo(this.calendarType)
-    return dateInfo.getInfoOfDate(currentDate)
+    const currentDate = new Date();
+    const dateInfo = new DateInfo(this.calendarType);
+    return dateInfo.getInfoOfDate(currentDate);
   }
 
   navigate(action: "next" | "back"){
+    switch (this.selectMode) {
+      case "years":
+        if(action == "next"){
+            this.yearsStep -= 1
+        } else if (action == "back"){
+            this.yearsStep += 1
+        }
+        break;
+
+      case "days":        
+        if(action == "next"){
+            if(this.selectedDate.month == this.months.length-1){
+              this.selectedDate.year += 1
+              this.selectedDate.month = 0
+            } else {
+              this.selectedDate.month! += 1
+            }
+
+            this.selectedDate.day = null
+          } else if (action == "back"){
+            if(this.selectedDate.month == 0){
+              this.selectedDate.year -= 1
+              this.selectedDate.month = 11
+            } else {
+              this.selectedDate.month! -= 1
+            }
+
+            this.selectedDate.day = null
+        }
+
+        this.selectedMonthDays = 
+        this.calandarConfig.getChunkedDaysInSelectedMonth(this.selectedDate.month!, this.selectedDate.year)
+        break;
+
+      case "months":        
+        if(action == "next"){
+            this.selectedDate.year += 1
+        } else if (action == "back"){
+          this.selectedDate.year -= 1
+        }
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  setValueForSelectView(viewMode: SelectModes, value: number){
+    switch (viewMode) {
+      case "years":
+        {
+          this.selectedDate.year = value          
+          this.selectedDate.month = null
+          this.selectMode = "months"
+        }
+        break;
+
+      case "months":
+        {
+          this.selectedDate.month = value
+          this.selectMode = "days"
+          this.selectedDate.day = null;
+
+          this.selectedMonthDays = 
+          this.calandarConfig.getChunkedDaysInSelectedMonth(this.selectedDate.month, this.selectedDate.year)
+        }
+        break;
+
+      case "days":
+        {
+          this.selectedDate.day = value          
+        }
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  closeDatePicker(){
+
+  }
+
+  saveIt(){
 
   }
 
