9e1ee39b7af5cb007e88293eeb56d0ba15339139
AmirReza Hammamian <amir.astar20@gmail.com>
2022-09-11 14:10:07 +0430

    refactor: replace and use from defaultsDeep

--- lib/src/datepicker/ngx-date-picker.directive.ts
+++ lib/src/datepicker/ngx-date-picker.directive.ts
@@ -1,7 +1,8 @@
 import { ComponentRef, Directive, ElementRef, EventEmitter, HostListener, Inject, Input, Output, ViewContainerRef } from '@angular/core';
-import { CalendarType, DATEPICKER_CONFIG, DefaultGlobalConfig, GlobalConfig, IDate, ThemeConfig, ValueFormat } from '../config/datePicker-config';
+import { DATEPICKER_CONFIG, DeepPartial, GlobalConfig, IDate } from '../config/datePicker-config';
 import { DatePickerHandler } from "../handlers";
 import DateTransform from '../helpers/dateTransform';
+import { DefaultsDeep } from '../helpers/functions';
 
 @Directive({
   selector: '[ngx-date-picker]',
@@ -15,10 +16,7 @@ export class NgxDatePickerDirective {
   private inputElement!: HTMLInputElement;
   private datePickerComponentElement!: ComponentRef<any> | null;
 
-  
-  @Input("datePickerConfig") datePickerConfig: Partial<GlobalConfig> = {};
-  @Input("datePickerType") calendar!: CalendarType;
-  @Input("datePickerFormat") outPutFormat!: ValueFormat;
+  @Input("datePickerConfig") datePickerConfig: DeepPartial<GlobalConfig> = {};
   @Output("onDateSelect") onSelect = new EventEmitter<Partial<IDate>>()
 
   constructor(
@@ -27,44 +25,18 @@ export class NgxDatePickerDirective {
     private el: ElementRef,
     private dpHandler: DatePickerHandler
   ) {
-
+    
     this.inputElement = this.el.nativeElement;
+    
     this.inputElement.onfocus = () => {   
-
-      console.log({...{rounded: DefaultGlobalConfig.themeConfig.rounded},
-        ...{rounded: this.datePickerConfig.themeConfig?.rounded}});
-      
-      let themeConfig: Partial<ThemeConfig> = {
-        ...{rounded: DefaultGlobalConfig.themeConfig.rounded},
-        ...{rounded: this.datePickerConfig.themeConfig?.rounded},
-        light: {
-          ...DefaultGlobalConfig.themeConfig.light,
-          ...this.datePickerConfig.themeConfig?.light,
-        },
-        dark: {
-          ...DefaultGlobalConfig.themeConfig.dark,
-          ...this.datePickerConfig.themeConfig?.dark
-        },
-      }
-          
-      const config = {
-        ...DefaultGlobalConfig,
-        ...this.datePickerConfig,
-        themeConfig
-      }
-
-      console.log(config);
-      
-      // const config: GlobalConfig = {
-      //   ...token,
-      //   calendar: this.calendar || DefaultGlobalConfig.calendar,
-      //   format: this.outPutFormat || DefaultGlobalConfig.format
-      // }
       
       if(!this.dpHandler.datePickerIsOpened){
-        this.datePickerComponentElement = this.dpHandler.init(this.inputElement, config, this._viewContainerRef)      
-        this.datePickerComponentElement.instance.onDateSelect.subscribe((date: Date) => {
 
+        const config: GlobalConfig = DefaultsDeep(this.datePickerConfig, token)
+        
+        this.datePickerComponentElement = this.dpHandler.init(this.inputElement, config, this._viewContainerRef)      
+
+        this.datePickerComponentElement.instance.onDateSelect.subscribe((date: Date) => {
           const dateTransform = new DateTransform(date, config.calendar)
           const inputValue = dateTransform.transformDate(config.format)
           this.inputElement.value = inputValue
--- lib/src/datepicker/ngx-date-picker.module.ts
+++ lib/src/datepicker/ngx-date-picker.module.ts
@@ -2,8 +2,8 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxDatePickerComponent } from './ngx-date-picker.component';
 import { NgxDatePickerDirective } from './ngx-date-picker.directive';
-import { DATEPICKER_CONFIG, GlobalConfig, DefaultGlobalConfig, ThemeConfig } from '../config/datePicker-config';
-
+import { DATEPICKER_CONFIG, GlobalConfig, DefaultGlobalConfig, ThemeConfig, DeepPartial } from '../config/datePicker-config';
+import { DefaultsDeep } from '../helpers/functions';
 
 @NgModule({
   declarations: [
@@ -19,30 +19,35 @@ import { DATEPICKER_CONFIG, GlobalConfig, DefaultGlobalConfig, ThemeConfig } fro
 })
 
 export class NgxDatePickerModule {
-  static forRoot(config: Partial<GlobalConfig> = {}): ModuleWithProviders<NgxDatePickerModule> {
+  static forRoot(config: DeepPartial<GlobalConfig> = {}): ModuleWithProviders<NgxDatePickerModule> {
     
-    console.log({...{rounded: DefaultGlobalConfig.themeConfig.rounded},
-      ...{rounded: config.themeConfig?.rounded}});
-    let themeConfig: Partial<ThemeConfig> = {
-      ...{rounded: DefaultGlobalConfig.themeConfig.rounded},
-      ...{rounded: config.themeConfig?.rounded},
-      light: {
-        ...DefaultGlobalConfig.themeConfig.light,
-        ...config.themeConfig?.light,
-      },
-      dark: {
-        ...DefaultGlobalConfig.themeConfig.dark,
-        ...config.themeConfig?.dark
-      },
-    }
+    // console.log({...{rounded: DefaultGlobalConfig.themeConfig.rounded},
+    //   ...{rounded: config.themeConfig?.rounded}});
+    // console.log(config);
+
+    const value: GlobalConfig = DefaultsDeep(config, DefaultGlobalConfig)
+    
+      
+    // let themeConfig: Partial<ThemeConfig> = {
+    //   ...{rounded: DefaultGlobalConfig.themeConfig.rounded},
+    //   ...{rounded: config.themeConfig?.rounded},
+    //   light: {
+    //     ...DefaultGlobalConfig.themeConfig.light,
+    //     ...config.themeConfig?.light,
+    //   },
+    //   dark: {
+    //     ...DefaultGlobalConfig.themeConfig.dark,
+    //     ...config.themeConfig?.dark
+    //   },
+    // }
         
-    console.log(themeConfig);
+    // console.log(themeConfig);
     
-    const value = {
-      ...DefaultGlobalConfig,
-      ...config,
-      themeConfig
-    }
+    // const value = {
+    //   ...DefaultGlobalConfig,
+    //   ...config,
+    //   themeConfig
+    // }
 
     return {
       ngModule: NgxDatePickerModule,
