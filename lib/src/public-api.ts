dbb524a6c308fc521932b6657eb8d0555c6c1bbc
AmiReza Hammamian <amir.astar20@gmail.com>
2022-09-18 20:32:49 +0430

    feat: export CustomConfig from lib

--- lib/src/public-api.ts
+++ lib/src/public-api.ts
@@ -6,3 +6,5 @@ export * from './datepicker/ngx-date-picker.service';
 export * from './datepicker/ngx-date-picker.component';
 export * from './datepicker/ngx-date-picker.module';
 export * from './datepicker/ngx-date-picker.directive';
+
+export { CustomConfig, ValueFormat } from './config/datePicker-config'
